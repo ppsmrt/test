e67b34b9be31e8d24e5dec881dd6d6dac4a59404
ppsmrt <124156458+ppsmrt@users.noreply.github.com>
2025-08-08 09:53:22 +0530

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -1,8 +1,8 @@
 // js/footer.js
 const footer = `
   <footer class="bg-white border-t mt-10 p-4 text-center text-sm text-gray-600">
-    <p class="font-semibold text-gray-700">TamilGeo V1</p>
-    <p class="text-gray-500">Powered By <span class="text-green-600 font-medium">Inteksoft Solutions</span></p>
+    <p class="font-semibold text-gray-700">TamilGeo V2.0 — User UI: Enhanced — Auto: Yes</p>
+    <p class="text-gray-500">Powered By <span class="text-green-600 font-medium">Inteksoft Solutions @ 2025</span></p>
   </footer>
 `;
 
